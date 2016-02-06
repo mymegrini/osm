336a039067506c7a882bfb2e34f6d324c7b0071c
Younes Megrini <rasetoi@yahoo.fr>
2016-02-06 11:52:32 +0100

    libparse : added printing and parsing of xml nodes
    added functions : printNode printBounds parseNode parseBounds
    added structure : osmNode osmBounds

--- lib/parse.c
+++ lib/parse.c
@@ -4,58 +4,120 @@
 #include <libxml/parser.h>
 #include "parse.h"
 
+  
 void
-parseStory (xmlDocPtr doc, xmlNodePtr cur) {
+printBounds (xmlNodePtr cur) {
+  
+  xmlChar *aval;
+  
+  puts("Bounds:");
 
-	xmlChar *key;
-	cur = cur->xmlChildrenNode;
-	while (cur != NULL) {
-	    if ((!xmlStrcmp(cur->name, (const xmlChar *)"keyword"))) {
-		    key = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
-		    printf("keyword: %s\n", key);
-		    xmlFree(key);
- 	    }
-	cur = cur->next;
-	}
-    return;
+  aval = xmlGetProp(cur, (const xmlChar*)"minlat");
+  printf("minlat: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"maxlat");
+  printf("maxlat: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"minlon");
+  printf("minlon: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"maxlon");
+  printf("maxlon: %s\n", aval);
+  free(aval);
+
+  return;
 }
 
 void
+printNode(xmlNodePtr cur) {
+
+  xmlChar* aval;
+
+  puts("Node:");
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"id");
+  printf("id: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"lat");
+  printf("lat: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"lon");
+  printf("lon: %s\n", aval);
+  free(aval);
+  
+  aval = xmlGetProp(cur, (const xmlChar*)"visible");
+  printf("visible: %s\n", aval);
+  free(aval);
+
+  return;
+}
+
+void
+parseNode(xmlNodePtr cur, osmNode* node) {
+  node->id = 0;
+  node->lat = 0;
+  node->lon = 0;
+  node->visible = 1;
+
+  return;
+}
+
+void
+parseBounds(xmlNodePtr cur, osmBounds* bounds) {
+  bounds->minlat = 0;
+
+  return;
+}
+
+
+void
 parseDoc(char *docname) {
+  
+  xmlDocPtr doc;
+  xmlNodePtr cur;
+  
+  doc = xmlParseFile(docname);
+  
+  if (doc == NULL ) {
+    fprintf(stderr,"Document not parsed successfully. \n");
+    return;
+  }
+  
+  cur = xmlDocGetRootElement(doc);
+  
+  if (cur == NULL) {
+    fprintf(stderr, "Empty document\n");
+    xmlFreeDoc(doc);
+    return;
+  }
+  
+  if (xmlStrcmp(cur->name, (const xmlChar *) "osm")) {
+    fprintf(stderr,"Document of the wrong type, root node != osm\n");
+    xmlFreeDoc(doc);
+    return;
+  }
+  
+  cur = cur->xmlChildrenNode;
+  while (cur != NULL) {
+    
+    if ((!xmlStrcmp(cur->name, (const xmlChar *)"bounds"))){  
+      printBounds(cur);
+    }
 
-	xmlDocPtr doc;
-	xmlNodePtr cur;
+    if ((!xmlStrcmp(cur->name, (const xmlChar *)"node"))){
+      printNode(cur);
+    }
 
-	doc = xmlParseFile(docname);
-	
-	if (doc == NULL ) {
-		fprintf(stderr,"Document not parsed successfully. \n");
-		return;
-	}
-	
-	cur = xmlDocGetRootElement(doc);
-	
-	if (cur == NULL) {
-		fprintf(stderr,"empty document\n");
-		xmlFreeDoc(doc);
-		return;
-	}
-	
-	if (xmlStrcmp(cur->name, (const xmlChar *) "osm")) {
-		fprintf(stderr,"document of the wrong type, root node != story\n");
-		xmlFreeDoc(doc);
-		return;
-	}
-	
-	cur = cur->xmlChildrenNode;
-	while (cur != NULL) {
-		if ((!xmlStrcmp(cur->name, (const xmlChar *)"storyinfo"))){
-			parseStory (doc, cur);
-		}
-		 
-	cur = cur->next;
-	}
-	
-	xmlFreeDoc(doc);
-	return;
+    cur = cur->next;
+  }
+  
+  puts("Parsing done.");
+
+  xmlFreeDoc(doc);
+  return;
 }
--- lib/parse.h
+++ lib/parse.h
@@ -1,7 +1,58 @@
 #include <libxml/xmlmemory.h>
 
-void
-parseStory(xmlDocPtr doc, xmlNodePtr cur);
+typedef struct {
+  uint32_t id;
+  double lat;
+  double lon;
+  uint32_t visible;
+} osmNode;
 
+typedef struct {
+  double minlat;
+  double maxlat;
+  double minlon;
+  double maxlon;
+} osmBounds;
+
+/**
+ * @brief This function prints attribute values for a Bounds node
+ * @param[in] cur xml Node pointer
+ * @return void
+ */
+void
+printBounds(xmlNodePtr cur);
+
+/**
+ * @brief This function prints attribute values for a Node node
+ * @param[in] cur xml Node pointer
+ * @return void
+ */
+void
+printNode(xmlNodePtr cur);
+
+/**
+ * @brief This function parses an osm Node
+ * @param[in] cur xml node pointer
+ * @param[out] node osmNode pointer
+ * @return void
+ */
+void
+parseNode(xmlNodePtr cur, osmNode* node);
+
+
+/**
+ * @brief This function parses an osm Node
+ * @param[in] cur xml node pointer
+ * @param[out] node osmNode pointer
+ * @return void
+ */
+void
+parseBounds(xmlNodePtr cur, osmBounds* node);
+
+/**
+ * @brief This function parses an osm file
+ * @param[in] docname filename of osm file
+ * @return void
+ */
 void
 parseDoc(char *docname);
