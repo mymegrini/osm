397503c93f067bbf152895a436619368f8c2941c
Younes Megrini <rasetoi@yahoo.fr>
2016-02-07 02:18:32 +0100

    lib/parse: added structures osm osmWay and osmRelation
    added structures mentioned above
    modified function prototypes to use new structures as needed
    discarded print functions to lib/print.c.h

--- lib/parse.h
+++ lib/parse.h
@@ -1,73 +1,123 @@
+#ifndef PARSE_H
+#define PARSE_H
+
 #include <libxml/xmlmemory.h>
 
+/**
+ * A structure to hold a 'tag' node
+ */
 typedef struct {
-  xmlChar* k;
-  xmlChar* v;
+  xmlChar* k;              /***< key >*/
+  xmlChar* v;              /***< value >*/
 } osmTag;
 
+/**
+ * A structure to hold a 'node' node or subtree
+ */
 typedef struct {
-  uint32_t id;
-  double lat;
-  double lon;
-  uint32_t tagListLength;
-  osmTag** tagList;
-  uint32_t visible;
+  uint32_t id;             /***< id number >*/
+  double lat;              /***< latitude coordinate >*/
+  double lon;              /***< longitude coordinate >*/
+  uint32_t tagc;           /***< tags cardinal >*/
+  osmTag** tagv;           /***< tags vector >*/
 } osmNode;
 
+/**
+ * A structure to hold the 'bounds' node
+ */
 typedef struct {
-  double minlat;
-  double maxlat;
-  double minlon;
-  double maxlon;
+  double minlat;           /***< latitude's lower bound >*/
+  double maxlat;           /***< latitude's upper bound >*/
+  double minlon;           /***< longitude's lower bound >*/
+  double maxlon;           /***< longitude's upper bound >*/
 } osmBounds;
 
+/**
+ * A structure to hold a 'way' subtree
+ */
 typedef struct {
-  uint32_t id;
-  uint32_t nodeListLength;
-  osmNode** nodeList;
-  uint32_t tagListLength;
-  osmTag** tagList;
+  uint32_t id;             /***< id number >*/
+  uint32_t nodec;          /***< nodes cardinal >*/
+  osmNode** nodev;         /***< nodes vector >*/
+  uint32_t tagc;           /***< tags cardinal >*/
+  osmTag** tagv;           /***< tags vector >*/
 } osmWay;
 
 /**
- * @brief This function prints attribute values for a Bounds node
- * @param[in] cur xml Node pointer
- * @return void
+ * A structure to hold a relation subtree
  */
-void
-printBounds(xmlNodePtr cur);
+typedef struct {
+  uint32_t id;             /***< id number >*/
+  uint32_t wayc;           /***< ways cardinal >*/
+  osmWay** wayv;           /***< ways vector >*/
+} osmRelation;
 
 /**
- * @brief This function prints attribute values for a Node node
- * @param[in] cur xml Node pointer
- * @return void
+ * A structure to hold an osm tree
  */
-void
-printNode(xmlNodePtr cur);
+typedef struct {
+  osmBounds* bounds;      /***< 'bounds' node >*/
+  uint32_t nodec;         /***< nodes cardinal >*/
+  osmNode** nodev;        /***< nodes vector >*/
+  uint32_t wayc;          /***< ways cardinal >*/
+  osmWay** wayv;          /***< ways vector >*/
+  uint32_t relationc;     /***< relations cardinal >*/
+  osmRelation** relationv;  /***< relations vector >*/
+} osm;
 
 /**
- * @brief This function parses an osm Node
+ * @brief This function parses a 'tag' node
  * @param[in] cur xml node pointer
- * @param[out] node osmNode pointer
+ * @param[out] tag 'tag' structure
  * @return void
  */
 void
-parseNode(xmlNodePtr cur, osmNode* node);
+parseTag(const xmlNodePtr cur, osmNode* tag);
+
+/**
+ * @brief This function parses a 'bounds' node
+ * @param[in] cur xml node pointer
+ * @param[out] bounds 'bounds' structure
+ * @return void
+ */
+void
+parseBounds(const xmlNodePtr cur, osmBounds* bounds);
+
+/**
+ * @brief This function parses a 'node' node
+ * @param[in] cur xml node pointer
+ * @param[out] node 'node' structure
+ * @return void
+ */
+void
+parseNode(const xmlNodePtr cur, osmNode* node);
 
 
 /**
- * @brief This function parses an osm Node
- * @param[in] cur xml node pointer
- * @param[out] node osmNode pointer
+ * @brief This function parses a 'way' subtree using a set of 'node' pointers
+ * @param[in] way 'way' structure
+ * @param[in] nodev 'node' vector
  * @return void
  */
 void
-parseBounds(xmlNodePtr cur, osmBounds* node);
+parseWay(const xmlNodePtr cur, const osmNode** nodev, osmWay* way);
+
+/**
+ * @brief This function parses a 'relation' node using a set of 'way' pointers
+ * @param[in] relation 'relation' structure
+ * @param[in] wayv 'way' vector
+ * @return void
+ */
+void
+parseRelation(const xmlNodePtr cur, const osmWay** wayv, osmRelation* relation);
 
 /**
  * @brief This function parses an osm file
+ * @param[in] map an osm tree structure
  * @param[in] docname filename of osm file
  * @return void
  */
 void
-parseDoc(char *docname);
+parseDoc(const char *docname, osm* map);
+
+#endif
