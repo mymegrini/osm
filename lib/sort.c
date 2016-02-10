38e5bcefdf5702448a6b8b23f23ab493f48e9c14
Younes Megrini <rasetoi@yahoo.fr>
2016-02-10 12:14:50 +0100

    lib/sort.c: added library for sorting structure vectors

---
+++ lib/sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void
+sortNodes(osmNode** nodev, uint32_t nodec){
+
+  
+  return;
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+osmNode*
+findNode(osmNode** nodev, uint32_t id){
+
+  return *nodev; //place holder
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+void
+sortWays(osmNode** wayv, uint32_t wayc){
+
+  return;
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+osmWay*
+findWay(osmWay** wayv, uint32_t id){
+
+  return *wayv; //place holder
+}
