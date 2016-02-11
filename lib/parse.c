ff2a1d3c703955563f264e98407d3298e65c7df7
Younes Megrini <rasetoi@yahoo.fr>
2016-02-11 11:05:05 +0100

    lib/parse: parseDoc: added long description, cleaned up code

--- lib/parse.c
+++ lib/parse.c
@@ -77,9 +77,16 @@ parseRelation(const xmlNodePtr cur, const osmWay** wayv, osmRelation* relation){
 				 (const xmlChar*)"true"))
 
 /**
- * 
- * 
- * 
+ * parseDoc parses the file \p docname using xmlParseFile then tests if parsing
+ * is successful, if the obtained tree is not empty and if its is named 'osm'.
+ * parseDoc then counts the number of 'node', 'way' and 'relation' nodes and
+ * allocates \p map vectors to the appropriate size. parseDoc then fills the 
+ * fields in the \p map structure by calling the appropriate parsing functions.
+ *
+ * @see parseBounds
+ * @see parseNode
+ * @see parseWay
+ * @see parseRelation
  */
 void
 parseDoc(const char *docname, osm* map) {
@@ -89,90 +96,71 @@ parseDoc(const char *docname, osm* map) {
   xmlNodePtr root;
   int node, way, relation;
 
-  //parse document using libxml
   doc = xmlParseFile(docname);
 
-  //test if parsing successful
   if (doc == NULL ) {
     fprintf(stderr,"Document not parsed successfully. \n");
     return;
   }
 
-  //obtain root node
   root = xmlDocGetRootElement(doc);
 
-  //test if document is not empty
   if (root == NULL) {
     fprintf(stderr, "Empty document\n");
     xmlFreeDoc(doc);
     return;
   }
 
-  //test if root node is of type osm
   if (xmlStrcmp(root->name, (const xmlChar *) "osm")) {
     fprintf(stderr,"Document of the wrong type, root node != osm\n");
     xmlFreeDoc(doc);
     return;
   }
 
-  //set counters
   way = 0;
   node = 0;
   relation = 0;
-  
-  //get first child
+
   cur = root->xmlChildrenNode;
   while (cur != NULL) {
 
-    //count nodes
     if (!xmlStrcmp(cur->name, (const xmlChar *)"node") && VISIBLE(cur)) node++;
 
-    //count ways
     if (!xmlStrcmp(cur->name, (const xmlChar *)"way") && VISIBLE(cur)) way++;
 
-    //count relations
     if (!xmlStrcmp(cur->name, (const xmlChar *)"relation") && VISIBLE(cur))
       relation++;
 
-    //get next child
     cur = cur->next;
   }
 
-  //alocate pointers and vectors
   map->bounds = (osmBounds*) malloc(sizeof(osmBounds));
   map->nodev = (osmNode**) malloc(node * sizeof(osmNode*));
   map->wayv = (osmWay**) malloc(way * sizeof(osmWay*));
   map->relationv = (osmRelation**) malloc(relation * sizeof(osmRelation*));
 
-  //reset counters
   way = 0;
   node = 0;
   relation = 0;
 
-  //get first child
   cur = root->xmlChildrenNode;
   while (cur != NULL) {
 
-    
-    //parse bounds node
     if (!xmlStrcmp(cur->name, (const xmlChar *)"bounds"))
       parseBounds(cur, map->bounds);
 
-    //parse node node
     if (!xmlStrcmp(cur->name, (const xmlChar *)"node") && VISIBLE(cur)){
       map->nodev[node] = (osmNode*) malloc(sizeof(osmNode));
       parseNode(cur, map->nodev[node]);
       node++;
     }
 
-    //parse way node
     if (!xmlStrcmp(cur->name, (const xmlChar *)"way") && VISIBLE(cur)){
       map->wayv[way] = (osmWay*) malloc(sizeof(osmWay));
       parseWay(cur, (const osmNode**)map->nodev, map->wayv[way]);
       way++;
     }
 
-    //parse relation node
     if (!xmlStrcmp(cur->name, (const xmlChar *)"relation") && VISIBLE(cur)){
       map->relationv[relation] = (osmRelation*) malloc(sizeof(osmRelation));
       parseRelation(cur, (const osmWay**)map->relationv,
@@ -180,16 +168,13 @@ parseDoc(const char *docname, osm* map) {
       relation++;
     } 
 
-    //get next child
     cur = cur->next;
   }
 
-  //fill counters
   map->nodec = node;
   map->wayc = way;
   map->relationc = relation;
 
-  //free opened document
   xmlFreeDoc(doc);
 
   return;
