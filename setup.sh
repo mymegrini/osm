6a9417e3454a704aacc11abcb6b0dca41f638bf6
Younes Megrini <rasetoi@yahoo.fr>
2016-02-11 11:10:43 +0100

    setup.sh: disabled apt-get calls

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install libxml2-dev
-sudo apt-get install libsdl2-dev
+#sudo apt-get install libxml2-dev
+#sudo apt-get install libsdl2-dev
 make
 export PATH="$PATH:$PWD"
 #export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/bin"
