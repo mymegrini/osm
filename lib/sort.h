0ca68368851a765accdf30e7eaf3e4f2d80ce068
Younes Megrini <rasetoi@yahoo.fr>
2016-02-13 11:18:08 +0100

    lib/parse.h: rearranged order

--- lib/sort.h
+++ lib/sort.h
@@ -4,15 +4,6 @@
 #include "parse.h"
 
 /**
- * @brief This function sorts a 'node' vector
- * @param[in] nodec node vector size
- * @param[out] nodev node vector
- * @return void
- */
-void
-sortNodes(osmNode** nodev, uint32_t nodec);
-
-/**
  * @brief This function finds a 'node' in a sorted 'node' vector
  * @param[in] id node id
  * @param[out] nodev node vector
@@ -22,15 +13,6 @@ osmNode*
 findNode(osmNode** nodev, uint32_t id);
 
 /**
- * @brief This function sorts a 'way' vector
- * @param[in] wayc way vector size
- * @param[out] wayv waye vector
- * @return void
- */
-void
-sortWays(osmNode** wayv, uint32_t wayc);
-
-/**
  * @brief This function finds a 'way' in a sorted 'way' vector
  * @param[in] id way id
  * @param[out] wayv way vector
@@ -38,5 +20,23 @@ sortWays(osmNode** wayv, uint32_t wayc);
  */
 osmWay*
 findWay(osmWay** wayv, uint32_t id);
+
+/**
+ * @brief This function sorts a 'node' vector
+ * @param[in] nodec node vector size
+ * @param[out] nodev node vector
+ * @return void
+ */
+void
+sortNodes(osmNode** nodev, uint32_t nodec);
+
+/**
+ * @brief This function sorts a 'way' vector
+ * @param[in] wayc way vector size
+ * @param[out] wayv waye vector
+ * @return void
+ */
+void
+sortWays(osmNode** wayv, uint32_t wayc);
 
 #endif
